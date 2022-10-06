4d06b8ed09c8cfe9d0d35cdd178eecca118bab1f
elvinHeo <shinilheo0205@gmail.com>
2022-10-06 22:20:04 +0900

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by ShinIl Heo on 2022/10/06.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
